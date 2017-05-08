d7945fd33e5678f56e99fa5e4f15a053a6ef4b8b
PaulNewman-M <[paulnewman.m@gmail.com]>
2017-05-07 19:24:22 -0500

    successfully added api using link:https://www.youtube.com/watch?v=fEYx8dQr_cQ&index=14&list=PLoYCgNOIyGACnrXwo5HMCfOH9VT05znGv

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 
-
   $(document).ready(function() {
     $("#Message").on("click", function(){
      $("#target").fadeToggle(500).fadeToggle(500);
@@ -20,3 +19,22 @@ $(function () {
   showText("#target1", "-by Paul Newman Mekala", 0, 200);   
 
 });
+
+
+$('button').click(function(){
+  randomQuote();
+});
+
+function randomQuote() {
+var $target= $("#target");
+  $.ajax({
+    
+      url: "http://api.forismatic.com/api/1.0/?",
+      dataType: "jsonp",
+      data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
+      success: function( response ) {
+        $target.html("<h4 id='o' class='text-center'>" +
+          response.quoteText + "</h4><p><br>&dash; " + response.quoteAuthor + "</p>");
+      }  
+  });
+}
